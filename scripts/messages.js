18606044a18d3af33aef1c6c9d912ca984f48410
bunkadis <47508417+bunkadis@users.noreply.github.com>
2019-02-28 20:38:26 -0500

    Update messages.js

--- scripts/messages.js
+++ scripts/messages.js
@@ -6,16 +6,25 @@ function createArticle( $message ) {
     let $message_author = $("<span>").addClass( "message-author").text( $message.user.name );
 
     let $dateMessage = new Date( Number.parseInt( $message.message.date ) );
-    let $dateTimeOnly = $dateMessage.toLocaleTimeString( );
-    let $dateDateOnly = $dateMessage.toLocaleDateString( );
+    let $dateDateOnly = new Intl.DateTimeFormat("en-us", {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      timeZone: "UTC"
+      }).format($dateMessage);
 
-    let $timePieces = $dateTimeOnly.replace( / /gi , ":" ).split( ":"); 
-    let $message_time = $("<span>").addClass( "message-time").text( $timePieces[ 0 ] + ":" + $timePieces[ 1 ] + " " +  $timePieces [ 3 ] + " " + $dateDateOnly);
+    let $dateTimeOnly = new Intl.DateTimeFormat("en-us", {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+      }).format($dateMessage);
+
+    let $message_time = $("<span>").addClass( "message-time").text($dateTimeOnly + " " + $dateDateOnly);
     console.log( "message-2" , $message );
-    let $message_text = $("<span>").addClass( "message-text").text( $message.message.text );      
+    let $message_text = $("<span>").addClass( "message-text").text( $message.message.text );
     let $message_meta_text = $("<div>").addClass( "message-meta-text");
     $message_meta_text.append( $message_author );
-    $message_meta_text.append( $message_time ); 
+    $message_meta_text.append( $message_time );
     $message_meta_text.append( $message_text );
 
     $article.append( $message_image );
@@ -37,7 +46,7 @@ function createArticle( $message ) {
         let $article = createArticle( message );
         $("#messages-list").append( $article );
       });
-    } 
+    }
 
   function submitMessage( message ) {
       $.post("/messages/new" , message , function(data, status){
@@ -62,14 +71,14 @@ function createArticle( $message ) {
          "text": event.target.value,
          "date": ( new Date( ) ).getTime( )
          }
-       };   
+       };
         let $article = createArticle( message );
-          $("#messages-list").prepend( $article );  
+          $("#messages-list").prepend( $article );
           event.target.value = "";
-        console.log( message );     
+        console.log( message );
         submitMessage( message );
       }
-     });        
+     });
 
     loadMessages( );
 
